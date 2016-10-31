fa0d07dcbead1898b3c2856da02cac7ce5daf6b7
Gabriel <Gabriel-p@users.noreply.github.com>
2016-10-30 23:00:38 -0300

    Update LatexWordStats.py

--- LatexWordStats.py
+++ LatexWordStats.py
@@ -5,6 +5,28 @@ import operator
 import re
 
 
+def read_file(f_name):
+    """
+    """
+    all_words = []
+    st_check = ("%", r"\begin{", r"\document", r"\use", r"\newco", r"\renew")
+    not_word = (
+        "texttt", "textbf", "emph", "hline", "citet", "citep", "simeq", "www")
+    with open(f_name) as f:
+        data = f.read()
+        for line in data.splitlines():
+            if not line.startswith(st_check) and line != '':
+                l_words = re.findall(r"[\w']+", line)
+                for word in l_words:
+                    word = word.lower().strip(r"""'.,;()[]{}:@""")
+                    if re.match(r'\w', word) and not num_there(word):
+                        if len(word) == 1 and word == 'a':
+                            all_words.append(word)
+                        elif len(word) > 1 and word not in not_word:
+                            all_words.append(word)
+    return all_words
+
+
 def num_there(s):
     """
     http://stackoverflow.com/a/19859340/1391441
@@ -12,34 +34,62 @@ def num_there(s):
     return any(i.isdigit() for i in s)
 
 
+def count_words(all_words):
+    """
+    """
+    wordcount = {}
+    for word in all_words:
+        if word not in wordcount:
+            wordcount[word] = 1
+        else:
+            wordcount[word] += 1
+    return wordcount
+
+
+def lenght_dist(wordcount):
+    """
+    """
+    lenghts = [0 for _ in range(15)]
+    for w, val in wordcount.items():
+        if len(w) < 15:
+            lenghts[len(w) - 1] += len(w) * val
+        else:
+            lenghts[14] += len(w) * val
+    return lenghts
+
+
 def main(f_name, max_w=20):
     """
     http://stackoverflow.com/a/21108583/1391441
     """
-    with open(f_name) as f:
-        wordcount = {}
-        for word in f.read().split():
-            word = word.lower().strip(r"""',.})]:@""")
-            if re.match(r'\w', word) and not num_there(word):
-                if word not in wordcount:
-                    wordcount[word] = 1
-                else:
-                    wordcount[word] += 1
-        # Reverse, put most used words on top.
-        w_sort = sorted(wordcount.items(), key=operator.itemgetter(1),
-                        reverse=True)
-        # Count total number of words.
-        w_sum = 0
-        for w in w_sort:
-            w_sum += w[1]
-        print("Words total: {}\n".format(w_sum))
-        # Print results.
-        p_sum = 0.
-        for i, w in enumerate(w_sort[:max_w]):
-            p = w[1] / float(w_sum) * 100.
-            p_sum += p
-            print("{:3d}. {:5d} ({:3.1f}%) - {}".format(i, w[1], p, w[0]))
-        print("\nSum percentage: {:.1f}%".format(p_sum))
+    # Read file and split into single words.
+    all_words = read_file(f_name)
+    # Count unique words.
+    wordcount = count_words(all_words)
+
+    # Reverse, put most used words on top.
+    w_sort = sorted(wordcount.items(), key=operator.itemgetter(1),
+                    reverse=True)
+    # Count total number of words.
+    w_sum = 0
+    for w in w_sort:
+        w_sum += w[1]
+    print("\nNumber of unique words: {}".format(len(w_sort)))
+    print("Total number of words: {}\n".format(w_sum))
+    # Print results.
+    p_sum = 0.
+    for i, w in enumerate(w_sort[:max_w]):
+        p = w[1] / float(w_sum) * 100.
+        p_sum += p
+        print("{:3d}. {:5d}     ({:3.2f}%) - {}".format(i, w[1], p, w[0]))
+    print("Sum percentage: {:.1f}%\n".format(p_sum))
+
+    print("Word lengths distribution:")
+    lenghts = lenght_dist(wordcount)
+    l_max = max(lenghts)
+    for i, le in enumerate(lenghts):
+        l_p = int(round(le * 30. / l_max))
+        print("{:2d}  ".format(i + 1) + "+" * l_p + " ({})".format(le))
 
 
 if __name__ == "__main__":
@@ -53,3 +103,4 @@ if __name__ == "__main__":
         main(args.n, args.m)
     else:
         main(args.n)
+
